07a290555d0a03e1564eee389c8a2d222ca11760
kimverlybustamante <kp.bustamantej@uea.edu.ec>
2026-09-22 16:47:30 -0500

    si

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -1,13 +1,17 @@
+import os
 import psycopg2
 
 
 def obtener_conexion():
-    conexion = psycopg2.connect(
+    database_url = os.environ.get("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(database_url)
+
+    return psycopg2.connect(
         host="localhost",
         port="5432",
         user="postgres",
         password="kookie",
         database="ferreteria"
     )
-
-    return conexion
